2d4fe217139a24795fac4f69edf75257e1e3fb06
danbart <danilo_solorzano06@hotmail.com>
2018-05-09 17:54:56 -0600

    update 9-05-18 18:00

--- application/models/Comunidades_Model.php
+++ application/models/Comunidades_Model.php
@@ -33,34 +33,51 @@ class Comunidades_Model extends CI_model
 
     public function comunFiestas($id = null){
         if (!is_null($id)){
-            $query = $this->db->query("select * from Comunidades cm inner join Fiestas ft on cm.idComunidades=ft.Comunidades_idComunidades where  cm.idComunidades=$id");
+          //LLAMAMOS FUNCIONES DEFINIDAS DENTRO DE LA CLASE
+            $fiestas = $this->getFiesta($id);
+            $comunidad =$this->get($id);
+            $semanasanta = $this->getSemanaSanta($id);
 
-            if ($query->num_rows()>0){
-                return $query->result_array();
-            }
-            return false;
+            $array = array($comunidad, $fiestas, $semanasanta);
+
+            return $array;
         }
-
-        $query = $this->db->query('select * from Comunidades cm inner join Fiestas ft on cm.idComunidades=ft.Comunidades_idComunidades');
-
-        if($query->num_rows()>0){
-          return $query->result_array();
+        $array = array();
+        $comunidad =$this->get();
+        //RECORREMOS EL ARREGLO DE COMUNIDADES Y LE AGREGAMOS LAS FIESTAS CORRESPONDIENTES
+        foreach($comunidad as $key => $value) {
+          $temp = $value['idComunidades'];
+          $temp = $temp +0;
+              $fiestas = $this->getFiesta($temp);
+              $semanasanta = $this->getSemanaSanta($temp);
+              array_push($array, $value, $fiestas, $semanasanta);
 
         }
-        return false;
+        return $array;
     }
 
     public function deparFiestas($id = null){
         if (!is_null($id)){
-            $query = $this->db->query("select * from departamentos dp inner join Comunidades cm on dp.idDepartamentos=cm.departamentos_idDepartamentos inner join Fiestas ft on cm.idComunidades=ft.Comunidades_idComunidades where  dp.idDepartamentos=$id");
+          $array = array();
+          //LLAMAMOS FUNCIONES DEFINIDAS DENTRO DE LA CLASE
+            $departamentos = $this->getDepartamento($id);
+            var_dump($departamentos);
+            foreach($departamentos as $key => $value) {
+              $temp1 = $value['idDepartamentos'];
+              $temp1 = $temp1+0;
+              var_dump($temp1);
+                  $comunidad =$this->getComunidad($temp);
+                  // $fiestas = $this->comunFiestas($id);
+                  array_push($array, $value, $comunidad);
 
-            if ($query->num_rows()>0){
-                return $query->result_array();
             }
-            return false;
+
+            // $array = array($comunidad, $fiestas);
+
+            return $array;
         }
 
-        $query = $this->db->query('select * from departamentos dp inner join Comunidades cm on dp.idDepartamentos=cm.departamentos_idDepartamentos inner join Fiestas ft on cm.idComunidades=ft.Comunidades_idComunidades');
+        $query = $this->db->query('select * from Departamentos dp inner join Comunidades cm on dp.idDepartamentos=cm.departamentos_idDepartamentos inner join Fiestas ft on cm.idComunidades=ft.Comunidades_idComunidades');
 
         if($query->num_rows()>0){
           return $query->result_array();
@@ -71,7 +88,7 @@ class Comunidades_Model extends CI_model
 
     public function getFiesta($id = null){
         if (!is_null($id)){
-            $query = $this->db->select('*')->from('fiestas')->where('Comunidades_idComunidades',$id)->get();
+            $query = $this->db->select('*')->from('Fiestas')->where('Comunidades_idComunidades',$id)->get();
 
             if ($query->num_rows()=== 1){
                 return $query->row_array();
@@ -81,7 +98,7 @@ class Comunidades_Model extends CI_model
             return false;
         }
 
-        $query = $this->db->select('*')->from('fiestas')->get();
+        $query = $this->db->select('*')->from('Fiestas')->get();
         if($query->num_rows()>0){
           return $query->result_array();
 
@@ -91,7 +108,7 @@ class Comunidades_Model extends CI_model
 
     public function getSemanaSanta($id = null){
         if (!is_null($id)){
-            $query = $this->db->select('*')->from('semanasanta')->where('_idComunidades',$id)->get();
+            $query = $this->db->select('*')->from('SemanaSanta')->where('_idComunidades',$id)->get();
 
             if ($query->num_rows()=== 1){
                 return $query->row_array();
@@ -101,7 +118,7 @@ class Comunidades_Model extends CI_model
             return false;
         }
 
-        $query = $this->db->select('*')->from('semanasanta')->get();
+        $query = $this->db->select('*')->from('SemanaSanta')->get();
         if($query->num_rows()>0){
           return $query->result_array();
 
@@ -109,4 +126,103 @@ class Comunidades_Model extends CI_model
         return false;
     }
 
+    public function getDepartamento($id = null){
+        if (!is_null($id)){
+            $query = $this->db->select('*')->from('Departamentos')->where('idDepartamentos',$id)->get();
+
+            if ($query->num_rows()=== 1){
+                return $query->row_array();
+            }elseif ($query->num_rows()> 1) {
+              return $query->result_array();
+            }
+            return false;
+        }
+
+        $query = $this->db->select('*')->from('Departamentos')->get();
+        if($query->num_rows()>0){
+          return $query->result_array();
+
+        }
+        return false;
+    }
+
+    public function getComunidad($id = null){
+        if (!is_null($id)){
+            $query = $this->db->select('*')->from('Comunidades')->where('departamentos_idDepartamentos',$id)->get();
+
+            if ($query->num_rows()=== 1){
+                return $query->row_array();
+            }elseif ($query->num_rows()> 1) {
+              return $query->result_array();
+            }
+            return false;
+        }
+
+        $query = $this->db->select('*')->from('Comunidades')->get();
+        if($query->num_rows()>0){
+          return $query->result_array();
+
+        }
+        return false;
+    }
+
+    public function getFotografiaFiesta($id = null){
+        if (!is_null($id)){
+            $query = $this->db->select('*')->from('Fotografia')->where('_idFiestas',$id)->get();
+
+            if ($query->num_rows()=== 1){
+                return $query->row_array();
+            }elseif ($query->num_rows()> 1) {
+              return $query->result_array();
+            }
+            return false;
+        }
+
+        $query = $this->db->select('*')->from('Fotografia')->get();
+        if($query->num_rows()>0){
+          return $query->result_array();
+
+        }
+        return false;
+    }
+
+    public function getFotografiaSemanaSanta($id = null){
+        if (!is_null($id)){
+            $query = $this->db->select('*')->from('Fotografia')->where('_idSemanaSanta',$id)->get();
+
+            if ($query->num_rows()=== 1){
+                return $query->row_array();
+            }elseif ($query->num_rows()> 1) {
+              return $query->result_array();
+            }
+            return false;
+        }
+
+        $query = $this->db->select('*')->from('Fotografia')->get();
+        if($query->num_rows()>0){
+          return $query->result_array();
+
+        }
+        return false;
+    }
+
+    public function getFotograFestaNacional($id = null){
+        if (!is_null($id)){
+            $query = $this->db->select('*')->from('Fotografia')->where('Fiestas_Nacionales_idFinac',$id)->get();
+
+            if ($query->num_rows()=== 1){
+                return $query->row_array();
+            }elseif ($query->num_rows()> 1) {
+              return $query->result_array();
+            }
+            return false;
+        }
+
+        $query = $this->db->select('*')->from('Fotografia')->get();
+        if($query->num_rows()>0){
+          return $query->result_array();
+
+        }
+        return false;
+    }
 }
